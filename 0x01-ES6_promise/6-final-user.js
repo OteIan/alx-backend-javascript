f36139ac71bbf56826490ba0693a352a355b48cc
OteIan <oteian9@gmail.com>
2024-03-05 13:54:23 +0300

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise'
+import uploadPhoto from './5-photo-reject'
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise.allSettled([
+        signUpUser(firstName, lastName),
+        uploadPhoto(fileName)
+    ])
+    .then((message) => (
+        message.map((i) => ({
+            status: i.status,
+            value: i.status === 'fulfilled' ? i.value : String(i.reason)
+        }))
+    ))
+}
