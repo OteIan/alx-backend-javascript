88cb7d26602e2c5268a0a4f9d2fcb219b91f9640
Ote Ian <oteian9@gmail.com>
2024-03-06 12:33:38 +0300

    Await / Async

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,5 +1,16 @@
 import { uploadPhoto, createUser } from './utils'
 
-export default function asyncUploadUser() {
-    
+export default async function asyncUploadUser() {
+    let msg = {};
+
+    try {
+        const photo = await uploadPhoto();
+        const user = await createUser();
+
+        msg = { photo, user };
+    } catch (err) {
+        msg = { photo: null, user: null };
+    }
+
+    return msg
 }
