8b30cc16a9b575984b331f30392d2c62c053a257
Ote Ian <oteian9@gmail.com>
2024-03-11 23:47:47 +0300

    Fix: ESlint

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 export default function getListStudentIds(students) {
   let result = [];
   if (students instanceof Array) {
-    result = students.map((student) => student.id)
+    result = students.map((student) => student.id);
   }
-  return result
+  return result;
 }
