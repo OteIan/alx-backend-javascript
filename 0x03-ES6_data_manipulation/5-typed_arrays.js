7c265f6127a81ee7b4619cca99f1926a29fde94a
Rich_ar_Dev <moriasrichar3@gmail.com>
2024-03-12 15:45:03 +0300

    Fix: Edge cases

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+
   const int8 = new ArrayBuffer(length);
   const dv = new DataView(int8);
 
